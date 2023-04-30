016e95a7a0d080305250d241284af58351e5794b
ninXout <58670749+ninXout@users.noreply.github.com>
2023-04-30 08:47:02 -0400

    Delete the main code because its getting cluttered

--- src/CrystalMenu.cpp
+++
@@ -1,9 +0,0 @@
-#include "CrystalClient.hpp"
-#include "Includes.hpp"
-#include "CrystalProfile.hpp"
-#include "Shortcuts.hpp"
-
-using namespace Shortcuts;
-using namespace Variables;
-using namespace Crystal;
-using namespace CrystalTheme;
--- src/globalBP.h
+++
@@ -1,4 +0,0 @@
-#include <vector>
-
-inline double g_fps = 60.0;
-void updateFPS(double spf);
